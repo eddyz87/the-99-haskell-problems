62e39880ad2b0adc325ef323e7515798dd2b0663
eddy <eddy@deadbeaf>
2014-06-19 00:52:04 +0300

    added solution for # 5,6

--- p1_10.hs
+++ p1_10.hs
@@ -40,4 +40,42 @@ testElementAt xs i = (i < 0) || (i >= length xs) || xs !! i == (elementAt xs i)
 
 -- 4
 
+myLength :: [a] -> Int
+myLength xs = len' xs 0
+  where len' [] i = i
+        len' (x:xs) i = len' xs $ i + 1
 
+doTestMyLength :: IO ()
+doTestMyLength = quickCheck testMyLength
+
+testMyLength :: [Int] -> Bool
+testMyLength xs = length xs == myLength xs
+
+-- 5
+
+myReverse :: [a] -> [a]
+myReverse xs = rev xs []
+  where rev [] acc = acc
+        rev (x:xs) acc = rev xs (x:acc)
+
+doTestMyReverse :: IO ()
+doTestMyReverse = quickCheck testMyReverse
+
+testMyReverse :: [Int] -> Bool
+testMyReverse xs = reverse xs == myReverse xs
+
+-- 6
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = xs == myReverse xs
+
+doTestIsPalindrome :: IO ()
+doTestIsPalindrome = do
+  quickCheck testIsPalindrome1
+  quickCheck testIsPalindrome2
+
+testIsPalindrome1 :: [Int] -> Bool
+testIsPalindrome1 xs = isPalindrome (xs ++ reverse xs) == True
+
+testIsPalindrome2 :: [Int] -> Bool
+testIsPalindrome2 xs = isPalindrome (1 : xs ++ [2]) == False
